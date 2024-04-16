c16db890b3da864b3b64b8d827ccbfb44cc758ac
Jen <jennifer.stemkowski@gmail.com>
2024-04-16 09:44:38 -0400

    Add FOR loops for even ind nums + sums

--- validator.py
+++ validator.py
@@ -1,6 +1,10 @@
 card_number = "5610591081018250"
 
+
 odd_sum = 0
+double_list = []
+even_sum = 0
+
 
 number = list(card_number)
 
@@ -8,6 +12,24 @@ for (idx, val) in enumerate(number):
     if idx % 2 != 0:
         odd_sum += int(val)
     else:
-        pass
+        double_list.append(int(val) * 2)
 
-print(odd_sum)
+
+double_string = ""
+
+for x in double_list:
+    double_string += str(x)
+
+
+double_list = list(double_string)
+
+for x in double_list:
+    even_sum += int(x)
+
+
+net_sum = odd_sum + even_sum
+
+if net_sum % 10 == 0:
+    print("Valid credit card")
+else:
+    print("Invalid credit card")
